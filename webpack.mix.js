59cabcb4c16b91d06404b968dcd761d7e82c9440
Gabriel Galan Mendez <gabrielgalanmendez@MacBook-Air-de-Gabriel.local>
2018-03-23 23:09:24 -0600

    changes in webpack

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,4 +13,4 @@ let mix = require('laravel-mix');
 
 mix.js('resources/assets/js/app.js', 'public/js')
   .stylus('resources/assets/stylus/app.styl', 'public/css')
-  .browserSync('http://localhost:8000/');
+  .browserSync('http://localhost:6000/');
